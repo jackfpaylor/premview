08df3c595146c8a97184fd6c2430132c0182dd22
jackfpaylor <108780425+jackfpaylor@users.noreply.github.com>
2023-05-30 14:36:40 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/about'
+  get 'home/pricing'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
